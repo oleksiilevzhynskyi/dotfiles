52d28d0a6c45b346e1be00a13210cc4bc4c9463a
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2015-12-02 23:00:46 +0200

    Install sublime as well.

--- install.rb
+++ install.rb
@@ -68,7 +68,9 @@ set_config ".zshrc"
 # check_and_install 'rvm' ''
 set_config ".pryrc"
 
-# #ssh config
-# # echo "Let's init ssh"
-# # mkdir -d "$HOME/.ssh"
-# # echo "..."
+install_app 'subl', <<-CMD
+  brew install caskroom/cask/brew-cask; brew tap caskroom/versions; brew cask install sublime-text3'
+  cd "$env:appdata\Sublime Text 3\Packages\"
+  rmdir -recurse User
+  cmd /c mklink /D User $env:userprofile\Dropbox\Sublime\User
+CMD
