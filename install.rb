cd42e7e280753c183745a36baa29b5c1b501403e
Oleksii Levzhynskyi <oleksii.levzhynskyi@gmail.com>
2022-11-20 10:45:02 +0200

    Update dotfiles #2

--- install.rb
+++ install.rb
@@ -1,19 +1,21 @@
 DOT_DIR = File.dirname(__FILE__)
-HOME = `echo $HOME`.gsub("\n", '')
+HOME = `echo $HOME`.strip!
 
 def install type, app, cmd
   case type
   when :app
     check = !system("which -s #{app}")
   when :path
-    check = File.exists?(app)
+    check = !File.exists?(app)
   end
 
   if check
     p "#{app} doesn't installed. Do you want to install it? [y/N]:"
-    input = gets
+    input = gets.strip!
+    p input
     if input == 'y' || input == 'Y'
-      `#{cmd}`
+      p cmd
+      system cmd
     end
   else
     p "#{app} already installed"
@@ -36,7 +38,7 @@ def set_config file
 
   if File.exists?(path)
     p "#{path} already exists. Do you want to overwrite it? [y/N]:"
-    input = gets
+    input = gets.strip!
     if input == 'y' || input == 'Y'
       cmd = "mv #{path}{,.bak}; #{cmd}"
       p cmd
@@ -51,9 +53,6 @@ end
 # brew
 install_app 'brew', 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
 
-# curl
-install_app 'curl', 'brew install curl'
-
 # git
 install_app 'git', 'brew install git'
 set_config ".gitconfig"
@@ -61,23 +60,34 @@ set_config ".gitignore_global"
 
 # zsh
 install_app 'zsh', 'brew install zsh zsh-completions'
-install_by_path '~/.oh-my-zsh', 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
+install_by_path "#{HOME}/.oh-my-zsh", 'sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"'
 set_config ".zshrc"
 
 # node
 install_app 'node', 'brew install npm'
 install_app 'npm', 'brew install npm'
-install_app 'nvm', 'brew install nvm'
-system 'nvm i; nvm alias default node'
+install_app 'nvm', 'source ~/.zshrc; brew install nvm;'
+# system 'nvm install; nvm alias default node'
 system 'npm i -g eslint jshint gulp nodemon grunt'
 
 # ruby
 # check_and_install 'rvm' ''
 set_config ".pryrc"
 
-install_app 'subl', <<-CMD
-  brew install caskroom/cask/brew-cask; brew tap caskroom/versions; brew cask install sublime-text3'
-  cd "$env:appdata\Sublime Text 3\Packages\"
-  rmdir -recurse User
-  cmd /c mklink /D User $env:userprofile\Dropbox\Sublime\User
+# projects
+system 'mkdir ~/projects'
+
+# setup Cask
+cmd = <<-CMD
+  brew install caskroom/cask/brew-cask;
+  brew tap caskroom/versions;
 CMD
+system cmd
+
+cmd = <<-CMD
+  brew cask install sublime-text3;
+  cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/;
+  rm -r User;
+  ln -s ~/Dropbox/Sublime/User;
+CMD
+install_app 'subl', cmd
